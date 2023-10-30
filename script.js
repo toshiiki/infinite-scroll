c569c8112ddc99fa98d44106cc0a342f653e06b4
toshiiki <133920784+toshiiki@users.noreply.github.com>
2023-10-29 22:24:29 -0400

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-console.log('testing');
+const inageContainer = document.getElementById('image-container');
+const loader =  document.getElementById('loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+const count = 30;
+const apiKey = 'fesULwHCSkJDvf-V_PFw5eilv5QWba_FA-CtWOB1UWc';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// check if all images were loaded
+function imageLoaded() {
+    imagesLoaded++;
+    if(imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+}
+
+// helper function to set attributes on DOM elements
+function setAttributes(element, attributes) {
+    for(const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// create elements for links & photos, add to dom
+function displayPhotos() {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
+    console.log('total images', totalImages);
+    // run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // create <a> to link to unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank'
+        })
+        // create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+        // event listener, check when each is finished loading
+        img.addEventListener('load', imageLoaded);
+        // put <img> inside <a> 
+        item.appendChild(img);
+        // put <a> inside image container
+        inageContainer.appendChild(item);
+    });
+}
+
+// get photos from unsplash api
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+    } catch (error) {
+
+    }
+}
+
+// check to see if scrolling near bottom of page, load more photos
+window.addEventListener('scroll', () => {
+    if(window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+        ready = false;
+        getPhotos();
+    }
+})
+
+// On load
+getPhotos();
